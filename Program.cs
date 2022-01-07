f55e1b640286ca0941fe08c223d33ab2b4167055
Ryan Grunest <ryangrunest@geeko>
2022-01-07 15:03:15 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// using System;
+
+class Hello {
+  static void Beans() {
+    Console.WriteLine("Hello, beans.");
+  }
+  static void Main() {
+    Console.WriteLine("Hello, World");
+    Beans();
+  }
+}
